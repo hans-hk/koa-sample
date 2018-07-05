21f5b8a5b7774c7e1161c55faea949627717b11b
hans <leekorea09@gmail.com>
2018-07-05 15:55:25 +0900

    eslint && pr setting

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true
+  },
+  'extends': 'eslint:recommended',
+  'parserOptions': {
+    'ecmaVersion': 2016,
+    'sourceType': 'module'
+  },
+  'parser': 'babel-eslint',
+  'rules': {
+    'indent': 0,
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'no-unused-vars': 0,
+    'no-console': 0,
+    'no-debugger': 0
+  }
+};
