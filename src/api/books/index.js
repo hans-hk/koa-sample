2f7fab93d6fde3cac9cc7f3811ce6e5861bce15c
hans <leekorea09@gmail.com>
2018-07-09 12:24:58 +0900

    book controller PUT, PATCH Method update

--- src/api/books/index.js
+++ src/api/books/index.js
@@ -7,7 +7,7 @@ books.get('/', booksCtrl.list);
 books.get('/:id', booksCtrl.get);
 books.post('/', booksCtrl.create);
 books.delete('/', booksCtrl.delete);
-books.put('/', booksCtrl.replace);
-books.patch('/', booksCtrl.update);
+books.put('/:id', booksCtrl.replace);
+books.patch('/:id', booksCtrl.update);
 
 module.exports = books;
