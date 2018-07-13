002234f44220969c8a8f3e358f617683ee6f39e8
hans <leekorea09@gmail.com>
2018-07-13 19:25:21 +0900

    koa-socket-io add

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ require('dotenv').config(); // .env 파일에서 환경변수 불러오기
 const { jwtMiddleware } = require('lib/token');
 
 const Koa = require('koa');
+const IO = require('koa-socket-2');
 const Router = require('koa-router');
 
 const app = new Koa();
+const io = new IO();
 const router = new Router();
 const api = require('./api');
 
@@ -14,9 +16,9 @@ const bodyParser = require('koa-bodyparser');
 mongoose.Promise = global.Promise; // Node 의 네이티브 Promise 사용
 // mongodb 연결
 mongoose.connect(process.env.MONGO_URI).then(
-    (response) => {
-      console.log('Successfully connected to mongodb');
-    }
+  (response) => {
+    console.log('Successfully connected to mongodb');
+  }
 ).catch(e => {
   console.error(e);
 });
@@ -28,6 +30,13 @@ app.use(jwtMiddleware);
 
 router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
 app.use(router.routes()).use(router.allowedMethods());
+
+
+io.attach(app);
+
+io.on('message', (ctx, data) => {
+  console.log('client send date to message endpoint', data);
+});
 
 app.listen(port, () => {
   console.log('heurm server is listening to port ' + port);
@@ -49,7 +58,7 @@ console.log(hashed);
 console.log('--------------');
 
 const jwt = require('jsonwebtoken');
-const token = jwt.sign({foo: 'bar'}, 'secret-key', {expiresIn: '7d'}, (err, token) => {
+const token = jwt.sign({ foo: 'bar' }, 'secret-key', { expiresIn: '7d' }, (err, token) => {
   if (err) {
     console.log(err);
     return;
